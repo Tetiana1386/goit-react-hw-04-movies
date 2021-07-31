848e57b049237b7126fed616c1da306fbacf5ee8
Tetiana1386 <tanyashostak13@gmail.com>
2021-08-01 01:08:35 +0300

    fix error

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 import errorImage from '../../img/no_result.jpeg';
 import './Error.css';
 
-function ErrorComponent({ texterror }) {
+function ErrorComponent({ message }) {
   return (
     <div role="alert" className="Wrapper">
       <img src={errorImage} width="550" alt="no_result" />
-      <p text={texterror} className="Text">
-        {texterror}
+      <p text={message} className="Text">
+        {message}
       </p>
     </div>
   );
